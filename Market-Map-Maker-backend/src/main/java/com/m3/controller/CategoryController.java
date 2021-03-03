93cd7908306c28e7ab18a66424144e35977a0190
Haben Zaid <haben.zaidt@gmail.com>
2021-03-02 19:14:32 -0500

    made endpoints accessible through cors origin annotation

--- Market-Map-Maker-backend/src/main/java/com/m3/controller/CategoryController.java
+++ Market-Map-Maker-backend/src/main/java/com/m3/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,6 +20,7 @@ import com.m3.repository.CategoryRepository;
 import com.m3.repository.CompanyRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/category")
 public class CategoryController {
 		
--- Market-Map-Maker-backend/src/main/java/com/m3/controller/MapController.java
+++ Market-Map-Maker-backend/src/main/java/com/m3/controller/MapController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,6 +22,7 @@ import com.m3.repository.CompanyRepository;
 import com.m3.repository.MapRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/map")
 public class MapController {
 	
